3d71f8d93410565eb5e1c64bff9b1d9faa431f44
Anna <ponchucha@yandex.ru>
2023-01-29 10:27:50 +0300

    add program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Написать программу, которая из имеющегося массива строк сформирует массив из строк, длина которых меньше, либо равна 3 символам.
+ Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.*/
+
+ // Метод разбиения строки на элементы массива по пробелам
+ string[] SplitStringToArray (string elements)
+  {
+    string[] array = elements.Split(' ', StringSplitOptions.RemoveEmptyEntries); // Убираем пустые подстроки, чтобы они не попали в результирующий массив
+    return array;
+ }
+
+ // Метод получения суммы строк, удовлетворяющих условию
+
+ string SumShortElements (string[]array, int maxStringLenght)
+ {
+    string sum = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            sum += array[i]+" ";
+        }
+    }
+    return sum;
+ }
+
+// Метод печати массива
+void PrintArray (string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length-1]}]");
+}
+
+Console.WriteLine("Введите элементы массива через пробел");
+string elements = Console.ReadLine() ??"";
+string[] arr = SplitStringToArray(elements);
+string sum = SumShortElements(arr, 3);
+string[] result = SplitStringToArray(sum);
+PrintArray(result);
